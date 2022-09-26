a38ad985aeb339561ae22276e3d0d6fa5a914c81
chiscocode <patrickchukwudifu716@gmail.com>
2022-09-26 12:41:11 +0100

    ready to deploy

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,7 @@ export default function Home() {
     <section className={styles.section}>
       <h2>Projects & Accomplishments</h2>
       <div className={styles.cardProject}>
-          <a href="https://pomonews24.com"><span>🏆 </span>Built an online news portal management system</a> &#8594;
+          <a href="https://pomonews24.com"><span>🏆 </span>Built an online news portal management system for pomonews24</a> &#8594;
 
           <div id={styles.technology}>
 
